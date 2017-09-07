5e4f18f99060fa76f90587300c7335c127d1a86e
Vincent Salamand <salamandvincent@gmail.com>
2017-09-07 18:54:37 +0200

    add recipe model

---
+++ db/migrate/20170907164846_create_recipes.rb
@@ -0,0 +1,12 @@
+class CreateRecipes < ActiveRecord::Migration[5.0]
+  def change
+    create_table :recipes do |t|
+      t.string :title
+      t.integer :servings
+      t.text :ingredients
+      t.text :instructions
+
+      t.timestamps
+    end
+  end
+end
