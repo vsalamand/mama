08de28b2125d3ce4460f703860496076f86816dd
Vincent Salamand <salamandvincent@gmail.com>
2017-09-21 17:40:20 +0200

    herbs

--- lib/tasks/ingredients_scraper.rb
+++ lib/tasks/ingredients_scraper.rb
@@ -29,6 +29,32 @@ class Scraper
     end
   end
 
+  def scrap_herbs_aromates
+    url = "https://fr.wikipedia.org/wiki/Herbes_et_aromates_de_cuisine"
+    file = open(url)
+    doc = Nokogiri::HTML(file)
+    herbs = []
+    doc.search("div[class='colonnes'] ul li a").each do |element|
+      herbs << element.text
+    end
+    File.open("../../db/herbes_aromates.json", 'wb') do |file|
+     file.write(JSON.generate(herbs))
+    end
+  end
+
+  def scrap_seasoning
+    url = "https://fr.wikipedia.org/wiki/Cat%C3%A9gorie:Condiment"
+    file = open(url)
+    doc = Nokogiri::HTML(file)
+    seasoning = []
+    doc.search("div[class='mw-category-group'] ul li a").each do |element|
+      seasoning << element.text
+    end
+    File.open("../../db/condiments.json", 'wb') do |file|
+     file.write(JSON.generate(seasoning))
+    end
+  end
+
   scraper = Scraper.new
-  scraper.scrap_vegetables
+  scraper.scrap_seasoning
 end
