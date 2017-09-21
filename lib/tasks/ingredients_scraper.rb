f01534e68cf66bc3a33c98e01a916ed04ee0e9ac
Vincent Salamand <salamandvincent@gmail.com>
2017-09-21 20:41:33 +0200

    cheese

--- lib/tasks/ingredients_scraper.rb
+++ lib/tasks/ingredients_scraper.rb
@@ -11,7 +11,7 @@ class Scraper
     doc.search('.mw-parser-output p a[title]').each do |element|
       vegetables << element.text
     end
-    File.open("../../db/vegetables.json", 'wb') do |file|
+    File.open("../../db/db_ingredients/vegetables.json", 'wb') do |file|
      file.write(JSON.generate(vegetables))
     end
   end
@@ -24,7 +24,7 @@ class Scraper
     doc.search("div[class='cell'] ol li").each do |element|
       fruits << element.text.gsub("\n",'').gsub("\t",'')
     end
-    File.open("../../db/fruits.json", 'wb') do |file|
+    File.open("../../db/db_ingredients/fruits.json", 'wb') do |file|
      file.write(JSON.generate(fruits))
     end
   end
@@ -37,7 +37,7 @@ class Scraper
     doc.search("div[class='colonnes'] ul li a").each do |element|
       herbs << element.text
     end
-    File.open("../../db/herbes_aromates.json", 'wb') do |file|
+    File.open("../../db/db_ingredients/herbes_aromates.json", 'wb') do |file|
      file.write(JSON.generate(herbs))
     end
   end
@@ -50,11 +50,99 @@ class Scraper
     doc.search("div[class='mw-category-group'] ul li a").each do |element|
       seasoning << element.text
     end
-    File.open("../../db/condiments.json", 'wb') do |file|
+    File.open("../../db/db_ingredients/condiments.json", 'wb') do |file|
      file.write(JSON.generate(seasoning))
     end
   end
 
+  def scrap_spices
+    url = "https://fr.wikipedia.org/wiki/Liste_d%27%C3%A9pices"
+    file = open(url)
+    doc = Nokogiri::HTML(file)
+    spices = []
+    doc.search("div[class='mw-parser-output'] ul li a").each do |element|
+      spices << element.text
+    end
+    File.open("../../db/db_ingredients/spices.json", 'wb') do |file|
+     file.write(JSON.generate(spices))
+    end
+  end
+
+  def scrap_pasta
+    url1 = "https://fr.wikipedia.org/wiki/Liste_des_p%C3%A2tes_courtes"
+    file1 = open(url1)
+    doc1 = Nokogiri::HTML(file1)
+    pasta = []
+    doc1.search("div[class='mw-parser-output'] ul li a").each do |element|
+      pasta << element.text
+    end
+    url2 = "https://fr.wikipedia.org/wiki/Liste_des_p%C3%A2tes_longues"
+    file2 = open(url2)
+    doc2 = Nokogiri::HTML(file2)
+    doc2.search("div[class='mw-parser-output'] ul li a").each do |element|
+      pasta << element.text
+    end
+    File.open("../../db/db_ingredients/pasta.json", 'wb') do |file|
+     file.write(JSON.generate(pasta))
+    end
+  end
+
+  def scrap_rice
+    url = "https://fr.wikipedia.org/wiki/Cat%C3%A9gorie:Vari%C3%A9t%C3%A9_de_riz"
+    file = open(url)
+    doc = Nokogiri::HTML(file)
+    rices = []
+    doc.search("div[class='mw-category'] ul li a").each do |element|
+      rices << element.text
+    end
+    File.open("../../db/db_ingredients/rices.json", 'wb') do |file|
+     file.write(JSON.generate(rices))
+    end
+  end
+
+# mauvais scraper
+  def scrap_bread
+    url = "https://fr.wikipedia.org/wiki/Pain"
+    file = open(url)
+    doc = Nokogiri::HTML(file)
+    breads = []
+    doc.search("div[class='mw-parser-output'] ul li a").each do |element|
+      breads << element.text
+    end
+    File.open("../../db/db_ingredients/pain.json", 'wb') do |file|
+     file.write(JSON.generate(breads))
+    end
+  end
+
+  def scrap_cheese
+    url1 = "https://fr.wikipedia.org/wiki/Fromage_au_lait_de_vache"
+    file1 = open(url1)
+    doc1 = Nokogiri::HTML(file1)
+    vache = []
+    doc1.search("div[class='mw-parser-output'] ul li a").each do |element|
+      vache << element.text
+    end
+    url2 = "https://fr.wikipedia.org/wiki/Fromages_au_lait_de_brebis"
+    file2 = open(url2)
+    doc2 = Nokogiri::HTML(file2)
+    brebis = []
+    doc2.search("div[class='mw-parser-output'] ul li a").each do |element|
+      brebis << element.text
+    end
+    url3 = "https://fr.wikipedia.org/wiki/Cat%C3%A9gorie:Fromage_au_lait_de_ch%C3%A8vre"
+    file3 = open(url3)
+    doc3 = Nokogiri::HTML(file3)
+    chevre = []
+    doc3.search("div[class='mw-category-group'] ul li a").each do |element|
+      chevre << element.text
+    end
+    File.open("../../db/db_ingredients/fromages.json", 'wb') do |file|
+     file.write(JSON.generate(vache))
+     file.write(JSON.generate(brebis))
+     file.write(JSON.generate(chevre))
+    end
+  end
+
   scraper = Scraper.new
-  scraper.scrap_seasoning
+  # scraper.scrap_cheese
 end
