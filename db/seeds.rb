27ec6895ed7c71d9ff3cf4f54686b370ce1aac44
Vincent Salamand <salamandvincent@gmail.com>
2017-09-22 19:17:29 +0200

    seed methods

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+
+def create_ingredient(name, tags)
+  @ingredient = Ingredient.new(name: name)
+  @ingredient.tag_list.add(tags, parse: true)
+  @ingredient.save
+end
+
+def update_ingredient(name, tags)
+  ingredient_id = Ingredient.where(name: name)[0].id
+  @ingredient = Ingredient.find(ingredient_id)
+  @ingredient.tag_list.add(tags, parse: true)
+  @ingredient.save
+end
+
+#boeuf
+filepath = 'db/db_ingredients/boeuf.json'
+serialized_boeuf = File.read(filepath)
+boeuf = JSON.parse(serialized_boeuf)
+boeuf.each do |element|
+  name = element
+  tags = "viande, boeuf"
+  Ingredient.exists?(name: name) ? update_ingredient(name, tags) : create_ingredient(name, tags)
+end
+
