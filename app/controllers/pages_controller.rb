7dea39deae42a12fe0fdfb36ec7688e6d2318d0c
Vincent Salamand <salamandvincent@gmail.com>
2017-10-05 18:46:39 +0200

    classifier beta

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -20,11 +20,12 @@ class PagesController < ApplicationController
   end
 
   def generate_recipe_items
-    recipe = Recipe.find(2)
+    recipe = Recipe.find(4)
     recipe_ingredients = recipe.ingredients.split("\r\n")
     recipe_ingredients.each do |element|
-      ingredient = Ingredient.search(element.tr("0-9", ""), operator: "or")
-      unit = Unit.search(element.tr("0-9", ""), operator: "or")
+      ingredient = Ingredient.search(element.tr("0-9", "").tr("'", " "), operator: "or")
+      element_less_ingredient = element.tr("0-9", "").downcase.split - ingredient[0]["name"].downcase.split
+      unit = Unit.search(element_less_ingredient.join(' '), operator: "or")
       quantity = element[/[+-]?([0-9]*[\D])?[0-9]+/]
       Item.create(ingredient: ingredient[0], unit: unit[0], quantity: quantity, recipe: recipe, recipe_ingredient: element)
     end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   validates :ingredient_id, presence: true
+  validates :ingredient_id, uniqueness: { scope: [:recipe_id, :unit_id] }
 
   belongs_to :ingredient
   belongs_to :recipe, optional: true
