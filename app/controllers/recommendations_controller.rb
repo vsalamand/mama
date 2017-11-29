e31c826ae2a35f6a44d83fbaa23d107e9b9e092b
Vincent Salamand <salamandvincent@gmail.com>
2017-11-29 18:36:32 +0100

    add daily reco generation and browsing

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -1,20 +1,20 @@
 require 'date'
 
 class RecommendationsController < ApplicationController
-  def create
+  def self.create(date)
     @recommendations = Recommendation.new
-    @recommendations.date = Date.today
-    @recommendations.daily_reco = recommend
-
+    @recommendations.recommendation_date = date
+    @recommendations.daily_reco = RecommendationsController.recommend
+    @recommendations.save
   end
 
   private
-  def recommend
+  def self.recommend
     suggestions = []
-    suggestions << Recipe.search("équilibré", fields: [:tags], where: {status: "published"}).to_a.shuffle.take(1)
-    suggestions << Recipe.search("rapide", fields: [:tags], where: {status: "published"}).to_a.shuffle.take(1)
-    suggestions << Recipe.search("léger", fields: [:tags], where: {status: "published"}).to_a.shuffle.take(1)
-    suggestions << Recipe.search("snack", fields: [:tags], where: {status: "published"}).to_a.shuffle.take(1)
-    suggestions << Recipe.search("gourmand", fields: [:tags], where: {status: "published"}).to_a.shuffle.take(1)
+    suggestions << Recipe.search("rapide", fields: [:tags], where: {status: "published"}).to_a.shuffle.take(1).first.id
+    suggestions << Recipe.search("léger", fields: [:tags], where: {status: "published"}).to_a.shuffle.take(1).first.id
+    suggestions << Recipe.search("snack", fields: [:tags], where: {status: "published"}).to_a.shuffle.take(1).first.id
+    suggestions << Recipe.search("gourmand", fields: [:tags], where: {status: "published"}).to_a.shuffle.take(1).first.id
+    return suggestions.join(', ')
   end
 end
