3b86e766af97614a881a78ac1bca47b8c131622e
Vincent Salamand <salamandvincent@gmail.com>
2017-12-13 15:57:14 +0100

    remove migration files: remove carts from item, drop carts, remove recommendable from recipes

--- db/migrate/20171027151208_remove_cart_from_items.rb
+++
@@ -1,5 +0,0 @@
-class RemoveCartFromItems < ActiveRecord::Migration[5.0]
-  def change
-    remove_reference :items, :cart, foreign_key: true
-  end
-end
--- db/migrate/20171109142639_remove_recommendable_from_recipes.rb
+++
@@ -1,5 +0,0 @@
-class RemoveRecommendableFromRecipes < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :recipes, :recommendable, :boolean
-  end
-end
