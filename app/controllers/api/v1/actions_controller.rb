4a38c93d2f1e1707218aa5f6e7d7a541038b2be6
Vincent Salamand <salamandvincent@gmail.com>
2018-02-07 18:28:22 +0100

    fix daily reco

--- app/controllers/api/v1/actions_controller.rb
+++ app/controllers/api/v1/actions_controller.rb
@@ -3,8 +3,8 @@ require 'date'
 class Api::V1::ActionsController < Api::V1::BaseController
 #http://localhost:3000/api/v1/suggest?date=-1
   def suggest
-    date = params[:date].present? ? (Date.today + params[:date].to_i).strftime("%F") : Date.today.strftime("%F")
-    RecommendationsController.create(date) if Recommendation.all.select { |reco| reco.recommendation_date == date }
+    date = params[:date].present? ? (Date.today + params[:date].to_i) : Date.today
+    RecommendationsController.create(date) unless Recommendation.where(recommendation_date: date).present?
     @suggestions = Recommendation.all.select { |reco| reco.recommendation_date == date }.first.daily_reco.split(',')
     respond_to do |format|
       format.json { render :suggest }
--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -1,3 +1,4 @@
 class Recommendation < ApplicationRecord
   validates :recommendation_date, :daily_reco, presence: true
+  validates :recommendation_date, uniqueness: true
 end
