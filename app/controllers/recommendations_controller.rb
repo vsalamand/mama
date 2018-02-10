3a9dfb3c6265541ac1a8d1ec2d8f518712950d6d
Vincent Salamand <salamandvincent@gmail.com>
2018-02-10 09:35:37 +0100

    add recommend action

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -9,13 +9,17 @@ class RecommendationsController < ApplicationController
   end
 
   private
-  def self.recommend
-    suggestions = []
-    suggestions << Recipe.search("rapide", fields: [:tags], where: {status: "published"}).to_a.shuffle.take(1).first.id
-    suggestions << Recipe.search("léger", fields: [:tags], where: {status: "published"}).to_a.shuffle.take(1).first.id
-    suggestions << Recipe.search("snack", fields: [:tags], where: {status: "published"}).to_a.shuffle.take(1).first.id
-    suggestions << Recipe.search("tarte salée", fields: [:tags], where: {status: "published"}).to_a.shuffle.take(1).first.id
-    suggestions << Recipe.search("gourmand", fields: [:tags], where: {status: "published"}).to_a.shuffle.take(1).first.id
-    return suggestions.join(', ')
+  def self.recommend(type)
+    if type
+     return recommendation = Recipe.search("#{type}", fields: [:tags], where: {status: "published"}).to_a.shuffle.take(1).first.id
+    else
+      suggestions = []
+      suggestions << Recipe.search("rapide", fields: [:tags], where: {status: "published"}).to_a.shuffle.take(1).first.id
+      suggestions << Recipe.search("léger", fields: [:tags], where: {status: "published"}).to_a.shuffle.take(1).first.id
+      suggestions << Recipe.search("snack", fields: [:tags], where: {status: "published"}).to_a.shuffle.take(1).first.id
+      suggestions << Recipe.search("tarte salée", fields: [:tags], where: {status: "published"}).to_a.shuffle.take(1).first.id
+      suggestions << Recipe.search("gourmand", fields: [:tags], where: {status: "published"}).to_a.shuffle.take(1).first.id
+      return suggestions.join(', ')
+    end
   end
 end
