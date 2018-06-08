f3b6973b830dafab6162067821de37449d4dbe67
Vincent Salamand <salamandvincent@gmail.com>
2018-06-08 13:01:36 +0200

    add diet model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180525140653) do
+ActiveRecord::Schema.define(version: 20180608105957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,13 @@ ActiveRecord::Schema.define(version: 20180525140653) do
     t.string   "schedule"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+  end
+
+  create_table "diets", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "food_list_items", force: :cascade do |t|
