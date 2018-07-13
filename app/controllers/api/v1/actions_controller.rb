e0e6e48d7a7b6debb57b2219205db77047b13249
Vincent Salamand <salamandvincent@gmail.com>
2018-07-13 12:24:14 +0200

    fix set default diet when asking for user diet and it's empty

--- app/controllers/api/v1/actions_controller.rb
+++ app/controllers/api/v1/actions_controller.rb
@@ -129,7 +129,7 @@ class Api::V1::ActionsController < Api::V1::BaseController
   #http://localhost:3000/api/v1/user_diet?user=12345678
   def user_diet
     @profile = User.find_by(sender_id: params[:user])
-    if @profile.diet = nil? then @profile.diet = Diet.first end
+    if @profile.diet == nil? then @profile.diet = Diet.first end
     respond_to do |format|
       format.json { render :user_diet }
     end
