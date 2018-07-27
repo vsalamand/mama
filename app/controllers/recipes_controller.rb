6524073bf112992d560d2feaffe94e2f9fa42ca7
Vincent Salamand <salamandvincent@gmail.com>
2018-07-27 18:55:02 +0200

    try with zoom option

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -12,7 +12,7 @@ class RecipesController < ApplicationController
         render pdf: "recipe_card",
                orientation: 'Landscape',
                page_size: 'A5',
-               ext_size_shrink:  0.8,
+               zoom:  0.4,
                margin:  {
                     top:               5,                     # default 10 (mm)
                     bottom:            0,
