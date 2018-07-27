b9a769cc06a4ecebc32a2d6647ea57e07603fd28
Vincent Salamand <salamandvincent@gmail.com>
2018-07-27 17:50:24 +0200

    try disable_smart_shrinking

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -12,7 +12,7 @@ class RecipesController < ApplicationController
         render pdf: "recipe_card",
                orientation: 'Landscape',
                page_size: 'A5',
-               dpi: '96',
+               disable_smart_shrinking: true,
                margin:  {
                     top:               5,                     # default 10 (mm)
                     bottom:            0,
