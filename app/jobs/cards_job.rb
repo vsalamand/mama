778200d30d877c1c6d1bd43620de24980b5842a7
Vincent Salamand <salamandvincent@gmail.com>
2018-07-28 14:04:54 +0200

    missing do in card job method

--- app/jobs/cards_job.rb
+++ app/jobs/cards_job.rb
@@ -2,7 +2,7 @@ class CardsJob < ApplicationJob
   queue_as :default
 
   def perform
-    Recipe.all.each |recipe|
+    Recipe.all.each do |recipe|
       Recipe.upload_to_cloudinary(recipe)
     end
   end
