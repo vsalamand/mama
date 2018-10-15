eddf3a81717e2e8df740c965608c335d77197f25
Vincent Salamand <salamandvincent@gmail.com>
2018-10-15 18:13:45 +0200

    refactor meta recipe creattion with collection singular ids methods to select categories

--- app/controllers/meta_recipes_controller.rb
+++ app/controllers/meta_recipes_controller.rb
@@ -8,6 +8,7 @@ end
 
 def create
   @meta_recipe = MetaRecipe.new(meta_recipe_params)
+  binding.pry
   @meta_recipe.ingredients = @meta_recipe.get_topping_ingredient if @meta_recipe.ingredients.empty?
   if @meta_recipe.save
     redirect_to meta_recipe_path(@meta_recipe)
@@ -21,7 +22,8 @@ private
     @meta_recipe = MetaRecipe.find(params[:id])
   end
 
+  # use collection_singular_ids "ids: []" to create multiple nested meta recipe list items
   def meta_recipe_params
-    params.require(:meta_recipe).permit(:name, :servings, :ingredients, :instructions, :meta_type, meta_recipe_list_items_attributes:[:name, :meta_recipe_list_id, :meta_recipe_id])
+    params.require(:meta_recipe).permit(:name, :servings, :ingredients, :instructions, :meta_type, meta_recipe_list_ids: [], meta_recipe_list_items_attributes:[:name, :meta_recipe_list_id, :meta_recipe_id])
   end
 end
--- app/javascript/components/selectize.js
+++ app/javascript/components/selectize.js
@@ -9,4 +9,4 @@ $(function() {
 });
 
 // Requiring CSS! Path is relative to ./node_modules
-import 'selectize/dist/css/selectize.default.css';
+import 'selectize/dist/css/selectize.bootstrap3.css';
