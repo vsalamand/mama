46ae6a54476563c4bac7d8dc936515dbba53e160
Vincent Salamand <salamandvincent@gmail.com>
2018-11-30 17:41:35 +0100

    add mising meal dashboard views + clean Mamajob to minimum tasks

--- app/jobs/mama_job.rb
+++ app/jobs/mama_job.rb
@@ -4,17 +4,7 @@ class MamaJob < ApplicationJob
   def perform
     # Update food lists
     FoodListJob.perform_now
-    # Update recipe lists
-    RecipeListJob.perform_now
-    # create recipe recommendations for the week
-    Diet.all.each do |diet|
-      RecommendationsController.create(diet)
-    end
-
-    # # update users weekly recommendations menu
-    # WeeklyMenuJob.perform_now
-
-    # create food checklists for the week after
-    ChecklistJob.perform_now
+    # Update recipes
+    Recipe.where(status: "published").each { |recipe| recipe.seasonal? }
   end
 end
