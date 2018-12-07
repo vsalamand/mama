5048f3c59a4121b9343611b153b7cb642b36cbb3
Vincent Salamand <salamandvincent@gmail.com>
2018-12-07 15:15:25 +0100

    foodlists refacto

--- app/jobs/food_list_job.rb
+++ app/jobs/food_list_job.rb
@@ -4,7 +4,7 @@ class FoodListJob < ApplicationJob
   def perform
     Food.reindex
     # Update ban diet food list if new food in database since last update
-    Diet.all.each { |diet| FoodList.update_banned_foods(diet) } if Food.where("updated_at > ?", DateTime.now - 7).any?
+    # Diet.all.each { |diet| FoodList.update_banned_foods(diet) } if Food.where("updated_at > ?", DateTime.now - 7).any?
     # update seasonal foods and create food categories
     FoodList.update_food_pools
   end
--- app/models/food_list.rb
+++ app/models/food_list.rb
@@ -144,19 +144,19 @@ class FoodList < ApplicationRecord
     end
   end
 
-  def self.update_banned_foods(diet)
-    banned_foods = FoodList.find_by(diet_id: diet.id, food_list_type: "ban")
+  # def self.update_banned_foods(diet)
+  #   banned_foods = FoodList.find_by(diet_id: diet.id, food_list_type: "ban")
 
-    # add food in foodlist unless already in
-    if diet.banned_categories.any?
-      diet.banned_categories.each do |banned_category|
-        banned_category.category.foods.each { |food| FoodListItem.find_or_create_by(name: food.name, food_id: food.id, food_list_id: banned_foods.id) }
-      end
-    end
-    if diet.banned_foods.any?
-      diet.banned_foods.each do |banned_food|
-        banned_food.food.subtree.each { |food| FoodListItem.find_or_create_by(name: food.name, food_id: food.id, food_list_id: banned_foods.id) }
-      end
-    end
-  end
+  #   # add food in foodlist unless already in
+  #   if diet.banned_categories.any?
+  #     diet.banned_categories.each do |banned_category|
+  #       banned_category.category.foods.each { |food| FoodListItem.find_or_create_by(name: food.name, food_id: food.id, food_list_id: banned_foods.id) }
+  #     end
+  #   end
+  #   if diet.banned_foods.any?
+  #     diet.banned_foods.each do |banned_food|
+  #       banned_food.food.subtree.each { |food| FoodListItem.find_or_create_by(name: food.name, food_id: food.id, food_list_id: banned_foods.id) }
+  #     end
+  #   end
+  # end
 end
