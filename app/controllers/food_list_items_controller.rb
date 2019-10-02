70a25189969c8d4d4657012605fc9f2383290053
Vincent Salamand <salamandvincent@gmail.com>
2019-10-02 10:30:42 +0200

    ajax method to add item to grocery lsit and update item card

--- app/controllers/food_list_items_controller.rb
+++ app/controllers/food_list_items_controller.rb
@@ -4,14 +4,14 @@ class FoodListItemsController < ApplicationController
 
 
   def create_foodlist_item
-    food = Food.find(params[:format])
+    @food = Food.find(params[:format])
     @foodlist = FoodList.find(params[:id])
 
-    current_item = @foodlist.food_list_items.find_by(food_id: food.id)
+    current_item = @foodlist.food_list_items.find_by(food_id: @food.id)
 
     unless current_item
-      FoodListItem.create(name: food.name, food_id: food.id, food_list_id: @foodlist.id)
-      head :ok
+      FoodListItem.create(name: @food.name, food_id: @food.id, food_list_id: @foodlist.id)
+      render "create_foodlist_item.js.erb"
     else
       redirect_back(fallback_location:"/")
     end
@@ -21,7 +21,7 @@ class FoodListItemsController < ApplicationController
     @foodlist = FoodList.find(params[:food_list_id])
     @foodlist_item.destroy
     respond_to do |format|
-      format.html { redirect_back(fallback_location:"/") }
+      # format.html { redirect_back(fallback_location:"/") }
       format.js   { render :layout => false }  # <-- will render `app/views/food_list_items/destroy.js.erb`
     end
   end
