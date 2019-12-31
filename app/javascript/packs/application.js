146d9bb389b156edf2fe6b21655ece7e4e55246d
Vincent Salamand <salamandvincent@gmail.com>
2019-12-31 13:01:48 +0100

    add JS to make bootstrap dropdowns with turbolinks

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -38,4 +38,22 @@ document.addEventListener('turbolinks:before-cache', () => {
 });
 
 
+// make bootstrap dropdowns work with turbolinks
+function toggleDropdown(element) {
+  var dropdown = new Dropdown(element);
+  dropdown.toggle();
+}
+
+document.addEventListener('turbolinks:load', function() {
+  var dropdown_buttons = document.querySelectorAll('[data-toggle="dropdown"]');
+
+  dropdown_buttons.forEach(function(element) {
+    element.addEventListener('click', function(event) {
+      event.preventDefault();
+
+      toggleDropdown(this);
+    });
+  });
+});
+
 
