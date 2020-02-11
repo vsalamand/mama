1fa05ef3479ff5a08a8bd67aca9d9e41807fcf5b
Vincent Salamand <salamandvincent@gmail.com>
2020-02-11 15:51:52 +0100

    fix migration file

--- db/migrate/20200210132247_additem_id_to_store_cart_item.rb
+++ db/migrate/20200210132247_additem_id_to_store_cart_item.rb
@@ -1,4 +1,4 @@
-class AddItemIdToStoreCartItems < ActiveRecord::Migration[5.0]
+class AdditemIdToStoreCartItem < ActiveRecord::Migration[5.0]
   def change
     add_reference :store_cart_items, :item, foreign_key: true, optional: true
   end
