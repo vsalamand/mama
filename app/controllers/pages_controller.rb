7cca86206d6597239b7ee43038f72b32ff61353b
Vincent Salamand <salamandvincent@gmail.com>
2020-02-27 11:09:42 +0100

    show recipe lists and carts index in modals

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,9 @@ class PagesController < ApplicationController
   def home
     @list = List.new
     @recipe_list = RecipeList.new
-    @recipes = RecipeList.where(recipe_list_type: "curated").last.recipes[0..5]
+    # @recipes = RecipeList.where(recipe_list_type: "curated").last.recipes[0..5]
+    @recipe_lists = current_user.recipe_lists.where(status: "opened")
+    @carts = current_user.carts.where.not(merchant: nil)
 
     # get user to the thank you page if not in beta
     if user_signed_in? && current_user.beta == false
