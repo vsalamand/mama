5f5c8c0b38a918dc4d126d9689a3c18855dea7d1
Vincent Salamand <salamandvincent@gmail.com>
2020-06-03 19:39:01 +0200

    prompt pwa install on iOS safari...

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -35,6 +35,25 @@ if (navigator.serviceWorker) {
     });
 }
 
+
+//  prompt install pwa popup on safari only
+// Detects if device is on iOS
+const isIos = () => {
+  const userAgent = window.navigator.userAgent.toLowerCase();
+  return /iphone|ipad|ipod/.test( userAgent );
+}
+// Detects if device is in standalone mode
+const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
+
+// Checks if should display install popup notification:
+if (isIos() && !isInStandaloneMode()) {
+  // this.setState({ showInstallMessage: true });
+  $('#isInStandaloneMode').modal('show')
+}
+
+
+
+
 //// Handle Turbolinks side-issues
 // clear the cache often
 $(document).on('ajax:before', '[data-remote]', () => {
