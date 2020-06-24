75a2459ef20aaa4a2ea06517c4fed31bb1b424fc
Vincent Salamand <salamandvincent@gmail.com>
2020-06-24 14:11:43 +0200

    fix typo in import recipe job

--- app/jobs/recipe_job.rb
+++ app/jobs/recipe_job.rb
@@ -12,7 +12,7 @@ class RecipeJob < ApplicationJob
         recipe.link = data["link"]
         recipe.status = "pending"
         recipe.scrape
-        Item.add_recipe_items(recipe) if recipe.save?
+        Item.add_recipe_items(recipe) if recipe.save
         puts "#{recipe.title}"
       end
     end
